f26d0fd8c49f902fa723451cf751d9b99cd669d1
Abhi Jain <devabhixda@gmail.com>
2020-12-21 11:26:45 +0530

    Improve chat list view

--- lib/Chat.dart
+++ lib/Chat.dart
@@ -80,37 +80,9 @@ class ChatRoomsTile extends StatelessWidget {
           )
         ));
       },
-      child: Container(
-        color: Colors.black26,
-        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-        child: Row(
-          children: [
-            Container(
-              height: 30,
-              width: 30,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30)),
-              child: Text(userName.substring(0, 1),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontFamily: 'OverpassRegular',
-                      fontWeight: FontWeight.w300)),
-            ),
-            SizedBox(
-              width: 12,
-            ),
-            Text(userName,
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontFamily: 'OverpassRegular',
-                    fontWeight: FontWeight.w300))
-          ],
-        ),
-      ),
+      contentPadding: EdgeInsets.symmetric(horizontal: 0.1 * MediaQuery.of(context).size.width),
+      leading: CircleAvatar(),
+      title: Text(userName),
     );
   }
 }
